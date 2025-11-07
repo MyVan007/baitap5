2355af19992e929dc1fbe6cc6f294c4b88e1d946
MyVan007 <myvanhp28@gmail.com>
2025-11-07 11:33:18 +0700

    van

---
+++ LineSub.java
@@ -0,0 +1,89 @@
+package bai5_1;
+
+public class LineSub extends Point {
+        private Point end;
+
+        public LineSub(int beginX, int beginY, int endX, int endY) {
+            super(beginX, beginY); // construct the begin Point
+            this.end = new Point(endX, endY); // construct the end Point
+        }
+
+        public LineSub(Point begin, Point end) {
+            super(begin.getX(), begin.getY());
+            this.end = end;
+        }
+
+        @Override
+        public String toString() {
+            return "LineSub[begin=(" + getX() + "," + getY() + "), end=(" + end.getX() + "," + end.getY() + ")]";
+        }
+
+        public Point getBegin() {
+            return new Point(getX(), getY());
+        }
+
+        public Point getEnd() {
+            return end;
+        }
+
+        public void setBegin(Point begin) {
+            this . setBeginXY(begin.getX(), begin.getY());
+        }
+
+        public void setEnd(Point end) {
+            this.end = end;
+        }
+
+        public int getBeginX() {
+            return getX();
+        }
+
+        public int getBeginY() {
+            return getY();
+        }
+
+        public int getEndX() {
+            return end.getX();
+        }
+
+        public int getEndY() {
+            return end.getY();
+        }
+
+        public void setBeginX(int x) {
+            setX(x);
+        }
+
+        public void setBeginY(int y) {
+            setY(y);
+        }
+
+        public void setBeginXY(int x, int y) {
+            setBeginXY(x, y);
+        }
+
+        public void setEndX(int x) {
+            end.setX(x);
+        }
+
+        public void setEndY(int y) {
+            end.setY(y);
+        }
+
+        public void setEndXY(int x, int y) {
+            end.setBeginXY(x, y);
+        }
+
+        public int getLength() {
+            int xDiff = end.getX() - getX();
+            int yDiff = end.getY() - getY();
+            return (int) Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+
+        public double getGradient() {
+            int xDiff = end.getX() - getX();
+            int yDiff = end.getY() - getY();
+            return Math.atan2(yDiff, xDiff);
+        }
+    }
+
---
+++ TestLine.java
@@ -0,0 +1,15 @@
+package bai5_1;
+
+public class TestLine {
+    public static void main(String[] args) {
+        Line l1 = new Line(0, 0, 3, 4);
+        System.out.println(l1);
+
+        Point p1 = new Point(100,200);
+        Point p2 = new Point(200,100);
+        Line l2 = new Line(p1, p2);
+        System.out.println(l2);
+
+    }
+
+}
